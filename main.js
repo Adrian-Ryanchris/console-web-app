16cc75bcae199194ed9cda543a3dded1272ebef1
Adrian-Ryanchris <139995001+Adrian-Ryanchris@users.noreply.github.com>
2024-03-13 22:28:46 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var first_name = "Adrian"
+var last_name = "Arokiyaraj"
+var fullname = first_name.concat(last_name);
+console.log(fullname);
+var number1 = 5
+var number2 = 10
+var add = number1+number2
+var multiply = number1 * number2
+console.log(multiply)
+var divide = number2 / number1
+console.log(divide);
+var subtraction = number2 - number1
+console.log(subtraction);
+console.log(add);
+console.log("My name is console screen");
